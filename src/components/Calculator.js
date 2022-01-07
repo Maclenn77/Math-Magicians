a2ab411ec154eb5628bd98bd4a3c103d7aea497e
Maclenn77 <maclenn77@gmail.com>
2022-01-07 14:21:55 -0600

    Return class-based components to functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,33 @@
 import React from 'react';
 
-class Calculator extends React.Component {
-  render() {
-    return (
-      <div className="calculator">
-        <div className="calc-display">0</div>
-        <div className="calc-main">
-          <div className="btn">AC</div>
-          <div className="btn">+/-</div>
-          <div className="btn">%</div>
-          <div className="btn">7</div>
-          <div className="btn">8</div>
-          <div className="btn">9</div>
-          <div className="btn">4</div>
-          <div className="btn">5</div>
-          <div className="btn">6</div>
-          <div className="btn">1</div>
-          <div className="btn">2</div>
-          <div className="btn">3</div>
-          <div className="btn calc-zero">0</div>
-          <div className="btn btn-point">.</div>
-        </div>
-        <div className="calc-operators">
-          <div className="btn-operators">÷</div>
-          <div className="btn-operators">x</div>
-          <div className="btn-operators">-</div>
-          <div className="btn-operators">+</div>
-          <div className="btn-operators">=</div>
-        </div>
-      </div>
-    );
-  }
-}
+const Calculator = () => (
+  <div className="calculator">
+    <div className="calc-display">0</div>
+    <div className="calc-main">
+      <div className="btn">AC</div>
+      <div className="btn">+/-</div>
+      <div className="btn">%</div>
+      <div className="btn">7</div>
+      <div className="btn">8</div>
+      <div className="btn">9</div>
+      <div className="btn">4</div>
+      <div className="btn">5</div>
+      <div className="btn">6</div>
+      <div className="btn">1</div>
+      <div className="btn">2</div>
+      <div className="btn">3</div>
+      <div className="btn calc-zero">0</div>
+      <div className="btn btn-point">.</div>
+    </div>
+    <div className="calc-operators">
+      <div className="btn-operators">÷</div>
+      <div className="btn-operators">x</div>
+      <div className="btn-operators">-</div>
+      <div className="btn-operators">+</div>
+      <div className="btn-operators">=</div>
+    </div>
+
+  </div>
+);
 
 export default Calculator;
