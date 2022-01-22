b0b77a3d413e6414d71149890eb9b4194bc8f35a
Maclenn77 <maclenn77@gmail.com>
2022-01-22 14:24:19 -0600

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => (
+    <section className="home">
+      <main>
+        <h2>
+          Welcome to our page!
+        </h2>
+        <p>
+          Lorem ipsum
+        </p>
+  
+        <p>
+          lorem ipsum
+        </p>
+      </main>
+    </section>
+  );
+  export default Home;
